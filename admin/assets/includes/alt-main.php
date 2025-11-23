6bb086f8260fd3a8bad89785072390e31e223f39
Barbara K. Moore <webdev@glitchwizardsolutions.com>
2025-11-23 18:24:21 -0500

    Update admin menu - remove Accounting System, rename Blog to Blog System, Accounts to Member Accounts, Newsletters to Newsletter System

--- admin/assets/includes/alt-main.php
+++ admin/assets/includes/alt-main.php
@@ -108,14 +108,14 @@ function template_admin_header($title, $selected = 'dashboard', $selected_child 
     <!--Blog-->
    <a href= "' . $base_url . '/cms/index.php"' . ($selected == 'blog' ? ' class="selected"' : '') . ' title="Blog">
             <span class="icon"><svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z"/></svg></span>
-            <span class="txt">Blog</span>
+            <span class="txt">Blog System</span>
             <span class="note">' . ($blog_posts_total ? number_format($blog_posts_total) : 0) . '</span>
             </a>
 
    <!--Accounts-->
    <a href= "' . $base_url . '/client_accounts/account_dash.php"' . ($selected == 'accounts' ? ' class="selected"' : '') . ' title="Accounts">
             <span class="icon"><svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z"/></svg></span>
-            <span class="txt">Accounts</span>
+            <span class="txt">Member Accounts</span>
             <span class="note">' . ($accounts_total ? number_format($accounts_total) : 0) . '</span>
             </a>
             <div class="sub">
@@ -186,7 +186,7 @@ function template_admin_header($title, $selected = 'dashboard', $selected_child 
             <span class="icon">
                 <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" /></svg>
             </span>
-            <span class="txt">Newsletters</span>
+            <span class="txt">Newsletter System</span>
             <span class="note">' . number_format($newsletters_total) . '</span>
         </a>
         <div class="sub">
--- admin/assets/includes/main.php
+++ admin/assets/includes/main.php
@@ -143,13 +143,13 @@ function template_admin_header($title, $selected = 'dashboard', $selected_child 
     <!--Blog System-->
    <a href= "' . $base_url . '/blog/blog_dash.php"' . ($selected == 'blog' ? ' class="selected"' : '') . ' title="IT Blog">
             <span class="icon"><svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z"/></svg></span>
-            <span class="txt">Blog</span>
+            <span class="txt">Blog System</span>
             </a>
 
    <!--Accounts-->
    <a href= "' . $base_url . '/client_accounts/account_dash.php"' . ($selected == 'accounts' ? ' class="selected"' : '') . ' title="Accounts">
             <span class="icon"><svg width="15" height="15" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M144 0a80 80 0 1 1 0 160A80 80 0 1 1 144 0zM512 0a80 80 0 1 1 0 160A80 80 0 1 1 512 0zM0 298.7C0 239.8 47.8 192 106.7 192h42.7c15.9 0 31 3.5 44.6 9.7c-1.3 7.2-1.9 14.7-1.9 22.3c0 38.2 16.8 72.5 43.3 96c-.2 0-.4 0-.7 0H21.3C9.6 320 0 310.4 0 298.7zM405.3 320c-.2 0-.4 0-.7 0c26.6-23.5 43.3-57.8 43.3-96c0-7.6-.7-15-1.9-22.3c13.6-6.3 28.7-9.7 44.6-9.7h42.7C592.2 192 640 239.8 640 298.7c0 11.8-9.6 21.3-21.3 21.3H405.3zM224 224a96 96 0 1 1 192 0 96 96 0 1 1 -192 0zM128 485.3C128 411.7 187.7 352 261.3 352H378.7C452.3 352 512 411.7 512 485.3c0 14.7-11.9 26.7-26.7 26.7H154.7c-14.7 0-26.7-11.9-26.7-26.7z"/></svg></span>
-            <span class="txt">Accounts</span>
+            <span class="txt">Member Accounts</span>
             <span class="note">' . ($accounts_total ? number_format($accounts_total) : 0) . '</span>
             </a>
             <div class="sub">
@@ -163,19 +163,6 @@ function template_admin_header($title, $selected = 'dashboard', $selected_child 
  
 
             
-            
-<!--Accounting System-->
-           <a href= "' . $base_url . '/resource_system/accountings.php"' . ($selected == 'finances' ? ' class="selected"' : '') . ' title="Accounting System"> 
-              <i class="fa-solid fa-money-bill-alt"></i>
-               <span class="txt">Accounting System&nbsp;<span style="color:red"> * </span></span>
-               </a>
-            <div class="sub">
-                <a href= "' . $base_url . '/resource_system/accounting-view.php"' . ($selected == 'finances' && $selected_child == 'manage' ? ' class="selected"' : '') . '><span class="square" style="background:#6610f2"></span>Expenses</a>
-                <a href= "' . $base_url . '/resource_system/accountings.php"' . ($selected == 'finances' && $selected_child == 'report' ? ' class="selected"' : '') . '><span class="square" style="background:#6610f2"></span>Management</a>
-                <a href= "' . $base_url . '/resource_system/accountings.php"' . ($selected == 'finances' && $selected_child == 'list' ? ' class="selected"' : '') . '><span class="square" style="background:#6610f2"></span>Reports</a>
-            </div>
-            
-
 <!--Feedback - REMOVED: poll_system directory does not exist-->
 <!--Contract Approvals - REMOVED: contract_system directory does not exist-->
 <!--Newsletter System-->
@@ -183,7 +170,7 @@ function template_admin_header($title, $selected = 'dashboard', $selected_child 
             <span class="icon">
                 <svg width="18" height="18" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" /></svg>
             </span>
-            <span class="txt">Newsletters</span>
+            <span class="txt">Newsletter System</span>
             <span class="note">' . number_format($newsletters_total) . '</span>
         </a>
         <div class="sub">
