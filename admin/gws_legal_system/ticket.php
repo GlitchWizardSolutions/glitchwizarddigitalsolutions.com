8e8f77c9c5dbe26ec79729d3385aa8574bc22e37
Barbara K. Moore <webdev@glitchwizardsolutions.com>
2025-11-26 00:46:12 -0500

    edits

--- admin/gws_legal_system/ticket.php
+++ admin/gws_legal_system/ticket.php
@@ -52,7 +52,7 @@ if (isset($_GET['id'])) {
 ?>
 <?=template_admin_header($page . ' GWS Legal Requirement', 'ticketing', 'legal')?>
 
-<div class="content-title">
+<div class="content-title mb-3">
     <div class="title">
         <div class="icon alt">
             <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M14,2H6A2,2 0 0,0 4,4V20A2,2 0 0,0 6,22H18A2,2 0 0,0 20,20V8L14,2M18,20H6V4H13V9H18V20Z" /></svg>
