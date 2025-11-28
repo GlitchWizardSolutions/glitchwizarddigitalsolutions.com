51b7cc90e7de710b909ec076dbb2baedf473f0b7
Barbara K. Moore <webdev@glitchwizardsolutions.com>
2025-11-28 00:36:13 -0500

    Links work without excess slashed now

--- client-dashboard/assets/includes/navigation.php
+++ client-dashboard/assets/includes/navigation.php
@@ -4,7 +4,7 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
       <li class="nav-item" id='dashboard'>
-        <a class="nav-link" href="<?php echo $base_url; ?>/index.php">
+        <a class="nav-link" href="<?php echo site_menu_base ?>client-dashboard/index.php">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
@@ -27,28 +27,28 @@
             </a>
           </li> 
           <li id='submit-project'>
-            <a href="<?php echo $base_url; ?>/communication/project-submit-ticket.php" style='color:green'>
+            <a href="<?php echo site_menu_base ?>client-dashboard/communication/project-submit-ticket.php" style='color:green'>
               <i class="bi bi-circle"></i><span>Submit Project</span>
             </a>
           </li> 
           
           <li id='admin-projects'>
-            <a href="<?php echo $base_url; ?>/communication/project-review-responses.php" style='color:green'>
+            <a href="<?php echo site_menu_base ?>client-dashboard/communication/project-review-responses.php" style='color:green'>
               <i class="bi bi-circle"></i><span>View Projects</span>
             </a>
           </li> 
           <li id='client-tickets'>
-            <a href="<?php echo $base_url; ?>/communication/all-client-review.php" style='color:green'>
+            <a href="<?php echo site_menu_base ?>client-dashboard/communication/all-client-review.php" style='color:green'>
               <i class="bi bi-circle"></i><span>View Client Tickets</span>
             </a>
           </li> 
            <li id='submit-project'>
-            <a href="<?php echo $base_url; ?>/barb-resources/warranty-submit-ticket.php" style='color:green'>
+            <a href="<?php echo site_menu_base ?>client-dashboard/barb-resources/warranty-submit-ticket.php" style='color:green'>
               <i class="bi bi-circle"></i><span>Submit Warranty</span>
             </a>
           </li> 
            <li id='gws-legal'>
-            <a href="<?php echo $base_url; ?>/gws-legal/gws-legal-review-responses.php" style='color:green'>
+            <a href="<?php echo site_menu_base ?>client-dashboard/gws-legal/gws-legal-review-responses.php" style='color:green'>
               <i class="bi bi-circle"></i><span>GSW Legal</span>
             </a>
           </li> 
@@ -66,12 +66,12 @@
         </a>
         <ul id="blog-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li id='blog-home'>
-            <a href="<?php echo $base_url; ?>blog/index.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/blog/index.php">
               <i class="bi bi-circle"></i><span>All Articles</span>
             </a>
           </li>
           <li id='blog-search'>
-            <a href="<?php echo $base_url; ?>blog/search.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/blog/search.php">
               <i class="bi bi-circle"></i><span>Search</span>
             </a>
           </li>
@@ -87,7 +87,7 @@
             
           <li id='ticket'>
             <?php if (can_access_communication($account['access_level'])): ?>
-            <a href="<?php echo $base_url; ?>/communication/submit-ticket.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/communication/submit-ticket.php">
               <i class="bi bi-circle"></i><span>Submit New Ticket</span>
             </a>
             <?php else: ?>
@@ -98,7 +98,7 @@
           </li> 
           <li id='mytickets'>
             <?php if (can_access_communication($account['access_level'])): ?>
-            <a href="<?php echo $base_url; ?>/communication/review-responses.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/communication/review-responses.php">
               <i class="bi bi-circle"></i><span>My Tickets</span>
             </a>
             <?php else: ?>
@@ -139,7 +139,7 @@
         </a>
         <ul id="invoices-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
           <li id='myinvoices'>
-            <a href="<?php echo $base_url; ?>/client-invoices.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/client-invoices.php">
               <i class="bi bi-circle"></i><span>My Invoices</span>
             </a>
           </li>
@@ -153,17 +153,17 @@
         </a>
       <ul id="settings-nav" class="nav-content collapse" data-bs-parent="#sidebar-nav">
           <li>
-            <a href="<?php echo $base_url; ?>/users-profile-edit.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/users-profile-edit.php">
               <i class="bi bi-circle"></i><span>Member Profile</span>
             </a>
           </li>
           <li id='mybusiness'>
-            <a href="<?php echo $base_url; ?>/client-businesses.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/client-businesses.php">
               <i class="bi bi-circle"></i><span>Business Profiles</span>
             </a>
           </li> 
          <li id='acct'>
-            <a href="<?php echo $base_url; ?>/users-account-edit.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/users-account-edit.php">
               <i class="bi bi-circle"></i><span>Settings</span>
             </a>
           </li><!--/acct-->
@@ -188,12 +188,12 @@
         </a>
         <ul id="components-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
-            <a href="<?php echo $base_url; ?>/components-alerts.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/components-alerts.php">
               <i class="bi bi-circle"></i><span>Alerts</span>
             </a>
           </li><!--/alerts-->
           <li>
-            <a href="<?php echo $base_url; ?>/components-accordion.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/components-accordion.php">
               <i class="bi bi-circle"></i><span>Accordion</span>
             </a>
           </li>
@@ -203,57 +203,57 @@
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/components-breadcrumbs.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/components-breadcrumbs.php">
               <i class="bi bi-circle"></i><span>Breadcrumbs</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/components-buttons.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/components-buttons.php">
               <i class="bi bi-circle"></i><span>Buttons</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/components-cards.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/components-cards.php">
               <i class="bi bi-circle"></i><span>Cards</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/components-carousel.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/components-carousel.php">
               <i class="bi bi-circle"></i><span>Carousel</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/components-list-group.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/components-list-group.php">
               <i class="bi bi-circle"></i><span>List group</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/components-modal.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/components-modal.php">
               <i class="bi bi-circle"></i><span>Modal</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/components-tabs.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/components-tabs.php">
               <i class="bi bi-circle"></i><span>Tabs</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/components-pagination.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/components-pagination.php">
               <i class="bi bi-circle"></i><span>Pagination</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/components-progress.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/components-progress.php">
               <i class="bi bi-circle"></i><span>Progress</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/components-spinners.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/components-spinners.php">
               <i class="bi bi-circle"></i><span>Spinners</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/components-tooltips.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/components-tooltips.php">
               <i class="bi bi-circle"></i><span>Tooltips</span>
             </a>
           </li>
@@ -266,22 +266,22 @@
         </a>
         <ul id="forms-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
-            <a href="<?php echo $base_url; ?>/forms-elements.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/forms-elements.php">
               <i class="bi bi-circle"></i><span>Form Elements</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/forms-layouts.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/forms-layouts.php">
               <i class="bi bi-circle"></i><span>Form Layouts</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/forms-editors.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/forms-editors.php">
               <i class="bi bi-circle"></i><span>Form Editors</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/forms-validation.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/forms-validation.php">
               <i class="bi bi-circle"></i><span>Form Validation</span>
             </a>
           </li>
@@ -294,12 +294,12 @@
         </a>
         <ul id="tables-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
-            <a href="<?php echo $base_url; ?>/tables-general.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/tables-general.php">
               <i class="bi bi-circle"></i><span>General Tables</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/tables-data.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/tables-data.php">
               <i class="bi bi-circle"></i><span>Data Tables</span>
             </a>
           </li>
@@ -312,17 +312,17 @@
         </a>
         <ul id="charts-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
-            <a href="<?php echo $base_url; ?>/charts-chartjs.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/charts-chartjs.php">
               <i class="bi bi-circle"></i><span>Chart.js</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/charts-apexcharts.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/charts-apexcharts.php">
               <i class="bi bi-circle"></i><span>ApexCharts</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/charts-echarts.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/charts-echarts.php">
               <i class="bi bi-circle"></i><span>ECharts</span>
             </a>
           </li>
@@ -335,17 +335,17 @@
         </a>
         <ul id="icons-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
           <li>
-            <a href="<?php echo $base_url; ?>/icons-bootstrap.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/icons-bootstrap.php">
               <i class="bi bi-circle"></i><span>Bootstrap Icons</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/icons-remix.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/icons-remix.php">
               <i class="bi bi-circle"></i><span>Remix Icons</span>
             </a>
           </li>
           <li>
-            <a href="<?php echo $base_url; ?>/icons-boxicons.php">
+            <a href="<?php echo site_menu_base ?>client-dashboard/icons-boxicons.php">
               <i class="bi bi-circle"></i><span>Boxicons</span>
             </a>
           </li>
@@ -355,14 +355,14 @@
       <li class="nav-heading">Pages</li>
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="<?php echo $base_url; ?>/pages-faq.php">
+        <a class="nav-link collapsed" href="<?php echo site_menu_base ?>client-dashboard/pages-faq.php">
           <i class="bi bi-question-circle"></i>
           <span>F.A.Q</span>
         </a>
       </li><!-- End F.A.Q Page Nav -->
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="<?php echo $base_url; ?>/pages-contact.php">
+        <a class="nav-link collapsed" href="<?php echo site_menu_base ?>client-dashboard/pages-contact.php">
           <i class="bi bi-envelope"></i>
           <span>Contact</span>
         </a>
@@ -371,7 +371,7 @@
    
 
       <li class="nav-item">
-        <a class="nav-link collapsed" href="<?php echo $base_url; ?>/pages-error-404.php">
+        <a class="nav-link collapsed" href="<?php echo site_menu_base ?>client-dashboard/pages-error-404.php">
           <i class="bi bi-dash-circle"></i>
           <span>Error 404</span>
         </a>
